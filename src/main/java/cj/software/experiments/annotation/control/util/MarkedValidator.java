654c0b53ef79fb4fb06f4e41840ba62826e78778
cjacobme <cjacobme@aol.com>
2023-09-19 21:13:34 +0200

    validate only the fields marked with @Summary

---
+++ src/main/java/cj/software/experiments/annotation/control/util/MarkedValidator.java
@@ -0,0 +1,27 @@
+package cj.software.experiments.annotation.control.util;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MarkedValidator {
+    private final MarkedFieldsCollector markedFieldsCollector = new MarkedFieldsCollector();
+
+    private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+    private final Validator validator = factory.getValidator();
+
+    public <T> Set<ConstraintViolation<T>> validate(T object) {
+        Set<ConstraintViolation<T>> result = new HashSet<>();
+        Set<String> toBeValidateds = markedFieldsCollector.collect(object.getClass());
+        for (String toBeValidated : toBeValidateds) {
+            Set<ConstraintViolation<T>> fieldViolations = validator.validateProperty(object, toBeValidated);
+            result.addAll(fieldViolations);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/cj/software/experiments/annotation/control/util/MarkedValidatorTest.java
@@ -0,0 +1,99 @@
+package cj.software.experiments.annotation.control.util;
+
+import cj.software.experiments.annotation.control.entity.Item;
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+class MarkedValidatorTest {
+    private static MarkedValidator markedValidator;
+
+    @BeforeAll
+    static void createValidator() {
+        markedValidator = new MarkedValidator();
+    }
+
+    @Test
+    void itemValidOnlyForSummary() {
+        Item item = Item.builder()
+                .withDescription("description")
+                .withTotalPrice(13.14)
+                .build();
+        Set<ConstraintViolation<Item>> violations = markedValidator.validate(item);
+        assertThat(violations).as("violations").isEmpty();
+    }
+
+    @Test
+    void itemDescriptionMissing() {
+        Item item = Item.builder()
+                .withTotalPrice(13.14)
+                .build();
+        Set<ConstraintViolation<Item>> violations = markedValidator.validate(item);
+        List<ErrorMessage> errorMessages = toErrorMessages(violations);
+        assertThat(errorMessages)
+                .as("error messages")
+                .extracting("propertyPath", "messageTemplate", "invalidValue")
+                .containsExactlyInAnyOrder(tuple("description", "{jakarta.validation.constraints.NotBlank.message}", null));
+    }
+
+    @Test
+    void itemTotalPriceTooSmall() {
+        Item item = Item.builder()
+                .withDescription("description")
+                .withTotalPrice(-15.2)
+                .build();
+        Set<ConstraintViolation<Item>> violations = markedValidator.validate(item);
+        List<ErrorMessage> errorMessages = toErrorMessages(violations);
+        assertThat(errorMessages)
+                .as("error messages")
+                .extracting("propertyPath", "messageTemplate", "invalidValue")
+                .containsExactlyInAnyOrder(tuple("totalPrice", "{jakarta.validation.constraints.DecimalMin.message}", -15.2));
+    }
+
+    private <T> List<ErrorMessage> toErrorMessages(Set<ConstraintViolation<T>> violations) {
+        List<ErrorMessage> result = new ArrayList<>(violations.size());
+        for (ConstraintViolation<?> violation : violations) {
+            ErrorMessage errorMessage = new ErrorMessage(violation);
+            result.add(errorMessage);
+        }
+        return result;
+    }
+
+    private static class ErrorMessage implements Serializable {
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        private final String propertyPath;
+
+        private final String messageTemplate;
+
+        private final Object invalidValue;
+
+        ErrorMessage(ConstraintViolation<?> constraintViolation) {
+            this.propertyPath = constraintViolation.getPropertyPath().toString();
+            this.messageTemplate = constraintViolation.getMessageTemplate();
+            this.invalidValue = constraintViolation.getInvalidValue();
+        }
+
+        public String getPropertyPath() {
+            return propertyPath;
+        }
+
+        public String getMessageTemplate() {
+            return messageTemplate;
+        }
+
+        public Object getInvalidValue() {
+            return invalidValue;
+        }
+    }
+}
